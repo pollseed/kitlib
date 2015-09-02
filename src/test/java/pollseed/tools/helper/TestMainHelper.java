b0e9a7339663ff5b7ea0159016b3ed6b50c27489
pollseed <cb.so.devope+0001@gmail.com>
2015-09-03 01:24:26 +0900

    Add TestCase for MainHelper

--- src/test/java/pollseed/tools/helper/TestMainHelper.java
+++ src/test/java/pollseed/tools/helper/TestMainHelper.java
@@ -1,16 +1,86 @@
 package pollseed.tools.helper;
 
+import junit.framework.Assert;
+
 import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+
 import pollseed.tools.helper.abst.AbstractMainHelper;
 
-public final class TestMainHelper extends AbstractMainHelper {
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 
-    public static void main(String[] args) {
-        new TestMainHelper().run();
+public final class TestMainHelper extends AbstractMainHelper {
+    private static final String LINE_STR = "-------------";
+    private static final String TEST_MESSAGE = "hoge";
+    private static ByteArrayOutputStream byteArrayOutputStream;
+
+    @Test
+    public void 標準出力のテスト_改行なし() {
+        byteArrayOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(new BufferedOutputStream(byteArrayOutputStream)));
+        l(TEST_MESSAGE);
+        System.out.flush();
+        Assert.assertEquals(byteArrayOutputStream.toString(), TEST_MESSAGE);
     }
 
-    @Override
-    protected void run() {
+    @Test
+    public void 標準出力のテスト_改行あり() {
+        byteArrayOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(new BufferedOutputStream(byteArrayOutputStream)));
+
+        ln(TEST_MESSAGE);
+        System.out.flush();
+        Assert.assertEquals(byteArrayOutputStream.toString(), TEST_MESSAGE + System.lineSeparator());
+        System.out.close();
+    }
+
+    @Test
+    public void 標準出力のテスト_ライン_中身なし() {
+        byteArrayOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(new BufferedOutputStream(byteArrayOutputStream)));
+        lnLine();
+        System.out.flush();
+        Assert.assertEquals(byteArrayOutputStream.toString(), LINE_STR + System.lineSeparator());
+    }
+
+    @Test
+    public void 標準出力のテスト_ライン_中身あり() {
+        byteArrayOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(new BufferedOutputStream(byteArrayOutputStream)));
+        lnLine(TEST_MESSAGE);
+        System.out.flush();
+        Assert.assertEquals(byteArrayOutputStream.toString(), LINE_STR + System.lineSeparator() + TEST_MESSAGE + System.lineSeparator() + LINE_STR
+                + System.lineSeparator());
+    }
+
+    @Test
+    public void 標準出力のテスト_リスト() {
+        byteArrayOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(new BufferedOutputStream(byteArrayOutputStream)));
+        @SuppressWarnings("serial")
+        List<Object> list = new ArrayList<Object>() {
+            {
+                add("hoge1");
+                add("hoge2");
+            }
+        };
+        lnList(list);
+        System.out.flush();
+        Assert.assertEquals(byteArrayOutputStream.toString(), "hoge1" + System.lineSeparator() + "hoge2" + System.lineSeparator());
+    }
+
+    @Test
+    public void 標準出力のテスト_埋め込み文字() {
+        String word = printf("hoge%s", "hoge");
+        Assert.assertEquals(word, "hogehoge");
+    }
+
+    @Test
+    public void メイン実行処理() {
         boolean _isTestCaseValue = true;
 
         // メソッド内で判定をさせる。
@@ -24,6 +94,17 @@ public final class TestMainHelper extends AbstractMainHelper {
         if (_isTestCaseValue) {
             ln(testCase("case2"));
         }
+
+        // 面倒だからここまでエラーなく来たらOKとする
+        Assert.assertTrue(_isTestCaseValue);
+    }
+
+    public static void main(String[] args) {
+        new TestMainHelper().run();
+    }
+
+    @Override
+    protected void run() {
     }
 
     private String testCase(String value) {
