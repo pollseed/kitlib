e92c31cb6d79ba90de6a1dc4061462a7512d6b9e
pollseed <cb.so.devope+0001@gmail.com>
2015-10-23 18:40:19 +0900

    Update SavageException.java

--- src/main/java/pollseed/tools/exception/SavageException.java
+++ src/main/java/pollseed/tools/exception/SavageException.java
@@ -1,5 +1,9 @@
 package pollseed.tools.exception;
 
+/**
+ * どの処理であろうと必ず {@code throw} して下さい.<br>
+ * <b>※例外を噛み潰さないで下さい.</b>
+ */
 public class SavageException extends Exception {
     public SavageException(final String msg, final Throwable throwable) {
         super(msg, throwable);
