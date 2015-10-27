a3a69ba48ad185e2cf7724ba8aa3bf5c55f3efdf
pollseed <cb.so.devope+0001@gmail.com>
2015-10-27 15:53:14 +0900

    Create Root.java

---
+++ gof/src/main/abst/Root.java
@@ -0,0 +1,10 @@
+package src.main.abst;
+
+public abstract class Root {
+    public void addDirectory(final Root directory) {
+    }
+
+    public abstract String getName();
+
+    public abstract int getSize();
+}
