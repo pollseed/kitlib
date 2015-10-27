71f2e4645766133581413d18af39017f23c719ba
pollseed <cb.so.devope+0001@gmail.com>
2015-10-27 15:54:00 +0900

    Create AbstractMain.java

---
+++ gof/src/main/abst/AbstractMain.java
@@ -0,0 +1,9 @@
+package src.main.abst;
+
+import org.junit.Assert;
+
+public abstract class AbstractMain {
+    protected final void eq(Object a, Object b) {
+        Assert.assertEquals(a, b);
+    }
+}
