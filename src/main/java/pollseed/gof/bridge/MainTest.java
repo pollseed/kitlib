e667f6a159fce795520235cc866b81abc1bcf2a7
pollseed <cb.so.devope+0001@gmail.com>
2015-11-04 17:36:38 +0900

    Update MainTest.java

--- src/main/java/pollseed/gof/bridge/MainTest.java
+++ src/main/java/pollseed/gof/bridge/MainTest.java
@@ -1,6 +1,9 @@
 package src.main;
 
 // 既存では、Compartmentを実装していたが、その間に処理をはさみたいとき
+// ex.) 使う時
+// final Compartment c = new MainExecuter(new MainTest());
+// c.execute();
 public class MainTest extends LoggerWrapper {
     @Override
     public void execute() {
