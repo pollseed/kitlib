6eae09a345b285c956f1912efa22e8a03233d3eb
pollseed <cb.so.devope+0001@gmail.com>
2015-11-25 17:00:39 +0900

    Create AbstractTestAggregation.java

---
+++ src/tmp/fxxk/abst/AbstractTestAggregation.java
@@ -0,0 +1,39 @@
+package tmp.fxxk.abst;
+
+import java.io.PrintStream;
+
+import tmp.fxxk.abst.AbstractTestAggregation.ParseTest.ValidateProcess;
+
+public abstract class AbstractTestAggregation {
+    protected AbstractTestAggregation() {
+    }
+
+    protected static ParseTest __executer = null;
+
+    protected final static void ln(final Object obj) {
+        O.println(obj);
+    }
+
+    protected final static void ln(final Object obj, final ValidateProcess p) throws Exception {
+        ln(obj + " 【Start】");
+        try {
+            p.execute();
+        } catch (final Exception e) {
+            ln(obj + " 【Error】");
+            throw e;
+        }
+        ln(obj + " 【Finale】");
+    }
+
+    protected interface ParseTest {
+        void executeErrorProcess() throws Exception;
+
+        void executeProcess() throws Exception;
+
+        interface ValidateProcess {
+            void execute();
+        }
+    }
+
+    private static final PrintStream O = System.out;
+}
