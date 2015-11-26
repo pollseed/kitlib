f250aa083db87465f9091ab60fa147327704f27e
pollseed <cb.so.devope+0001@gmail.com>
2015-11-26 12:09:18 +0900

    Update AnnotationExecuterHelper.java

--- src/main/java/pollseed/tools/helper/interfaces/AnnotationExecuterHelper.java
+++ src/main/java/pollseed/tools/helper/interfaces/AnnotationExecuterHelper.java
@@ -1,26 +1,16 @@
 package pollseed.tools.helper.interfaces;
 
-
-import pollseed.tools.helper.abst.AnnotationController;
-import pollseed.tools.helper.abst.AnnotationExecuterHelperWrapper;
-
 /**
- * {@link AnnotationController#execute(AnnotationAction.AnnotationGenerator, Class)}
+ * {@link AnnotationController#annotationExecuter(Class, ExecuterHelper)}
  * 実行時の処理を分岐させたい場合にこちらにルールを追加してください.<br>
- * <b>※このインタフェースを使用せず、{@link AnnotationExecuterHelperWrapper} を継承して下さい.</b> <br>
- * <br>
- * このインタフェースは、開発者が使用するものではなくただのルールです.
- *
- * @deprecated
  */
-@Deprecated
+@FunctionalInterface
 public interface AnnotationExecuterHelper {
 
     /**
-     * {@link AnnotationAction.ProcessTimer}
+     * {@link ProcessTimer}
      *
-     * @deprecated
      */
-    @Deprecated
     void processTimerExecute();
+
 }
